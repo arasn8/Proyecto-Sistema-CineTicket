63fb24567b3bf8ff02aeb5e1461ee10b7cc31057
arasn8 <angelancalla@gmail.com>
2026-08-03 18:52:18 -0500

    Mejora de la interfaz visual, doficiacion de HomeController

--- CineTicket.Web/Controllers/HomeController.cs
+++ CineTicket.Web/Controllers/HomeController.cs
@@ -1,31 +1,25 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using CineTicket.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CineTicket.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly CineTicketContext _db;
+    public HomeController(CineTicketContext db) => _db = db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public async Task<IActionResult> Index()
     {
-        _logger = logger;
+        var estrenos = await _db.Peliculas
+            .Include(p => p.IdGeneroNavigation)
+            .Where(p => p.Estado)
+            .OrderByDescending(p => p.IdPelicula) // las agregadas mas recientemente = "estrenos"
+            .Take(5)
+            .ToListAsync();
+
+        return View(estrenos);
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    public IActionResult Error() => View();
 }
